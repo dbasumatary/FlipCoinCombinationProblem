e1eb9732d48f527f2c33cb6e9e354e3f21d11643
dbasumatary <basudhondip@gmail.com>
2023-01-21 18:50:01 +0530

    Add: Determine the percentage of the Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,12 @@
 echo "Welcome to Flip Coin Combination Problem"
 
-read -p "Enter the number of times to flip the singlet:" num
+read -p "Enter the number of times to flip the singlet: " num
 declare -A singletFlip
 
 singletFlip[H]=0
 singletFlip[T]=0
+echo -e "\n"
+echo "The following is calculation of singlet combination"
 
 for(( flip = 1; flip <= num; flip++ ))
 do
@@ -26,4 +28,47 @@ do
     percent=$(( ${singletFlip[$combination]}  * 100 / num ))
     singletFlip[$combination]=$percent
     echo "The percentage of $combination is ${singletFlip[$combination]}%"
+done
+
+echo -e "\n"
+
+declare -A doubletFlip
+
+doubletFlip[HH]=0
+doubletFlip[HT]=0
+doubletFlip[TT]=0
+doubletFlip[HH]=0
+echo "The following is calculation of doublet combination"
+
+for(( flip = 1; flip <= num; flip++ ))
+do
+	echo -n "Flip number of $flip is "
+        ((counter1 = RANDOM%2))
+        ((counter2 = RANDOM%2))
+
+        case $counter1$counter2 in
+        	00)
+            		echo "Heads Heads"
+            		(( doubletFlip[HH]++ ))
+            		;;
+        	01)
+            		echo "Heads Tails"
+            		(( doubletFlip[HT]++ ))
+            		;;
+        	10)
+            		echo "Tails Heads"
+            		(( doubletFlip[TH]++ ))
+            		;;
+        	11)
+            		echo "Tails Tails"
+            		(( doubletFlip[TT]++ ))
+            		;;
+    	esac
+done
+
+for combination in ${!doubletFlip[@]}
+do
+	percent=$(( ${doubletFlip[$combination]}  * 100 / num ))
+        doubletFlip[$combination]=$percent
+        echo "The percentage of $combination is ${doubletFlip[$combination]}%"
 done
